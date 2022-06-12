4a0c38f7679b570909b6b0696fab3f84a9739bad
DarkDav <dadasovdavid@gmail.com>
2022-06-12 15:39:04 +0300

    Uncheck2

--- src/main/java/ru/kata/Util/InitDefUsers.java
+++ src/main/java/ru/kata/Util/InitDefUsers.java
@@ -1,4 +1,4 @@
-package ru.kata.AddDefUsers;
+package ru.kata.Util;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
--- src/main/java/ru/kata/config/SecurityConfig.java
+++ src/main/java/ru/kata/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.kata.security;
+package ru.kata.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
